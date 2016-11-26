0f1bfd985e2b7eee56aff360f37e73526123b4d0
Outsider <outsideris@gmail.com>
2016-11-27 02:56:34 +0900

    migrate comment model

--- server/boot/create-lb-tables.js
+++ server/boot/create-lb-tables.js
@@ -1,7 +1,7 @@
 const server = require('../server');
 
 const ds = server.dataSources.pg;
-const lbTables = ['Post'];
+const lbTables = ['Post', 'Comment'];
 
 ds.automigrate(lbTables, (er) => {
   if (er) throw er;
