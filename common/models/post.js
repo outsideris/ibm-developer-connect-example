b9295766f44a02529caada7b53162c4e8c48bd6b
Outsider <outsideris@gmail.com>
2016-11-27 02:56:47 +0900

    expend post api

--- common/models/post.js
+++ common/models/post.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(Post) {
-
+  Post.commentCount = (postId, cb) => {
+    Post.findById(postId,
+      {include: {relation: 'comments'}}, (err, instance) => {
+        instance.comments.count((err, count) => {
+          cb(err, count);
+        });
+    });
+  },
+  Post.remoteMethod(
+    'commentCount',
+    {
+      http: {path: '/comment/count', verb: 'get'},
+      accepts: {arg: 'id', type: 'number', http: {source:'query'}},
+      returns: {arg: 'count', type: 'number'}
+    }
+  )
 };
